abf1cab04fb8aa7e5c1b3e71e2585e6c48845677
Hristina <hristina.kovacevic.srb@gmail.com>
2021-12-23 14:06:17 +0100

    add logo img

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import {BrowserRouter} from 'react-router-dom';
-import Navbar from './components/navbar/Navbar';
+import Navbar from './components/Navbar';
+
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
             <Navbar/>
             
-            
+           
             </BrowserRouter>
             
         </div>
