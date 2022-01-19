57e86f0b2acbfd1094f57f121f261144a2e6c240
RobertBognar <robertbognar2@gmail.com>
2022-01-19 13:17:19 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Register from './components/Register/Register';
 import Login from './components/Login/Login';
 import GuestHomepage from './components/guest-homepage/GuestHomepage';
 import ProfilePage from './components/my-profile-page/ProfilePage';
-
-
 import GuestLayout from './components/Layout/GuestLayout';
 import AuthLayout from './components/Layout/AuthLayout';
 import Page from './components/pages/Page';
@@ -52,6 +50,8 @@ function App() {
                     />
                 </Routes>
             </BrowserRouter>
-
+        </div>
+    );
+}
 
 export default App;
