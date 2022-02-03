f41919a28343d313feae36e3b9bf6750ff8e56f8
Hristina Kovacevic <hristina.kovacevic.srb@gmail.com>
2022-02-03 13:11:11 +0100

    useForm, add companyId, order

--- src/components/AddNewQuestion.js
+++ src/components/AddNewQuestion.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 import addQuestionService from '../services/AddQuestionService';
 import {
     Box,
@@ -11,61 +12,73 @@ import {
     Select,
 } from '@chakra-ui/react';
 const AddNew = () => {
-    const [question, setQuestion] = useState('');
-    const [option, setOption] = useState('');
+    const {
+        register,
+        handleSubmit,
+        getValues,
+        formState: { errors },
+    } = useForm();
 
     const saveQuestion = () => {
+        const question = getValues('question');
+        const option = getValues('questionType');
+
         addQuestionService.addQuestion(question, option);
-        console.log(question, option);
-        setQuestion('');
     };
 
     return (
         <Flex direction="column">
-            <Box
-                className="bgBlack"
-                width="100%"
-                direction={{ base: 'column', md: 'row' }}
-                justify={{ base: 'center' }}
-                alignItems="center"
-                ml="0 "
-                gap={10}
-                py={{ base: 14, sm: 14, md: 14, lg: 0 }}
-            >
-                <Flex direction="column" align="center">
-                    <Box width="60%" lineHeight="2em">
-                        <Text color="white">Add new Question</Text>
+            <form onSubmit={handleSubmit(saveQuestion)}>
+                <Box
+                    className="bgBlack"
+                    width="100%"
+                    direction={{ base: 'column', md: 'row' }}
+                    justify={{ base: 'center' }}
+                    alignItems="center"
+                    ml="0 "
+                    gap={10}
+                    py={{ base: 14, sm: 14, md: 14, lg: 0 }}
+                >
+                    <Flex direction="column" align="center">
+                        <Box width="60%" lineHeight="2em">
+                            <Text color="white">Add new Question</Text>
 
-                        <FormControl>
-                            <FormLabel color="whiteAlpha.700">
-                                Question text
-                            </FormLabel>
-                            <Input
+                            <FormControl>
+                                <FormLabel color="whiteAlpha.700">
+                                    Question text
+                                </FormLabel>
+                                <Input
+                                    color="white"
+                                    placeholder="Question text"
+                                    width="50%"
+                                    {...register('question', {
+                                        required: true,
+                                    })}
+                                />
+                            </FormControl>
+                            {errors.question && (
+                                <Text color="red">
+                                    Question text is required!
+                                </Text>
+                            )}
+                            <Text color="whiteAlpha.700">Question type</Text>
+                            <Select
                                 color="white"
-                                value={question}
-                                placeholder="Question text"
-                                width="50%"
-                                onChange={(e) => setQuestion(e.target.value)}
-                            />
-                        </FormControl>
-                        <Text color="whiteAlpha.700">Question type</Text>
-                        <Select
-                            value={option}
-                            color="white"
-                            width="30%"
-                            onChange={(e) => setOption(e.target.value)}
-                        >
-                            <option value="Text">Text</option>
-                            <option value="Long Text">Long Text</option>
-                            <option value="Image">Image</option>
-                        </Select>
-                    </Box>
+                                width="30%"
+                                {...register('questionType')}
+                            >
+                                <option value="text">Text</option>
+                                <option value="long_text">Long Text</option>
+                                <option value="image">Image</option>
+                            </Select>
+                        </Box>
 
-                    <Button colorScheme="gray" onClick={saveQuestion}>
-                        Save
-                    </Button>
-                </Flex>
-            </Box>
+                        <Button colorScheme="gray" onClick={saveQuestion}>
+                            Save
+                        </Button>
+                    </Flex>
+                </Box>{' '}
+            </form>
         </Flex>
     );
 };
--- src/services/AddQuestionService.js
+++ src/services/AddQuestionService.js
@@ -2,16 +2,22 @@ import http from './HttpService';
 
 const addQuestionService = {
     addQuestion: async function (addedQuestion, option) {
-        const data = {
-            text: addedQuestion,
-            type: option,
-            order: 0,
-        };
         try {
-            const response = await http.post('/questions', {
-                data: data,
-            });
-            console.log(response);
+            const questions = await http.get('/questions');
+            let order = questions.data.data.length;
+
+            console.log(order);
+            let userData = JSON.parse(localStorage.getItem('userData'));
+            let data = {
+                data: {
+                    text: addedQuestion,
+                    type: option,
+                    order: order + 1,
+                    company: userData.username,
+                },
+            };
+            const response = await http.post('/questions', data);
+            return response;
         } catch (error) {
             console.log('An error occurred:', error.message);
         }
