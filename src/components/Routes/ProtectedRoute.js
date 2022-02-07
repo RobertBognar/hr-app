48039b60cf309788d0489b8cea1d1851901303f4
RobertBognar <robertbognar2@gmail.com>
2022-02-07 14:08:37 +0100

    changes

--- src/components/Routes/ProtectedRoute.js
+++ src/components/Routes/ProtectedRoute.js
@@ -3,8 +3,8 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useAuthContext } from '../../context/AuthContext';
 function ProtectedRoute() {
     const { userToken } = useAuthContext();
-    
-    return userToken ? <Outlet /> : <Navigate to="/login" />;
+    // return userToken ? <Outlet /> : <Navigate to="/login" />;
+    return <Outlet />;
 }
 
 export default ProtectedRoute;
