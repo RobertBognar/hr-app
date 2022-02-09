9033ff27a39e3102b2c756a4ef1a9baa7b664bc8
Noni86kg <nonisc2@gmail.com>
2022-02-09 15:37:59 +0100

    add pagination on questionslistmain

---
+++ src/components/questions-list/PaginationQuestionsList.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Button, Text, Flex, Box, HStack } from '@chakra-ui/react';
+
+const PaginationQuestionsList = ({
+    postsPerPage,
+    totalPosts,
+    paginate,
+    currentPage,
+}) => {
+    const pageNumbers = [];
+
+    for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+
+    const handlePageNumber = (e) => {
+        if (e === 'previos') {
+            if (currentPage === 1) {
+                currentPage = 1;
+            } else {
+                paginate(currentPage - 1);
+            }
+        } else {
+            if (currentPage === pageNumbers.length) {
+                paginate(pageNumbers.length);
+            } else {
+                paginate(currentPage + 1);
+            }
+        }
+    };
+
+    return (
+        <HStack spacing="15px">
+            <Button
+                bg="black"
+                color="white"
+                border="1px"
+                borderColor="white"
+                _hover={{
+                    background: 'white',
+                    color: 'black',
+                }}
+                onClick={() => handlePageNumber('previos')}
+            >
+                Previous
+            </Button>
+            {pageNumbers.map((number) => {
+                return (
+                    <Box key={number}>
+                        <Text
+                            key={number}
+                            onClick={() => paginate(number)}
+                            color={currentPage === number ? 'black' : 'white'}
+                            cursor="pointer"
+                            textAlign="center"
+                            w="25px"
+                            border="1px"
+                            borderColor="white"
+                            bg={currentPage === number ? 'white' : ''}
+                        >
+                            {number}
+                        </Text>
+                    </Box>
+                );
+            })}
+            <Button
+                bg="black"
+                color="white"
+                border="1px"
+                borderColor="white"
+                _hover={{
+                    background: 'white',
+                    color: 'black',
+                }}
+                onClick={() => handlePageNumber('next')}
+            >
+                Next
+            </Button>
+        </HStack>
+    );
+};
+
+export default PaginationQuestionsList;
--- src/components/questions-list/QuestionsList.js
+++ src/components/questions-list/QuestionsList.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
 import { Heading, Text, Flex, Button } from '@chakra-ui/react';
 import { SmallAddIcon } from '@chakra-ui/icons';
 import { useNavigate } from 'react-router-dom';
 import questionsListService from '../../services/QuestionsListService';
+import PaginationQuestionsList from './PaginationQuestionsList';
 
 const QuestionsList = () => {
     const [data, setData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage] = useState(4);
 
     async function questionArrayFunc() {
         const arr = await questionsListService.questionsData();
@@ -23,6 +25,14 @@ const QuestionsList = () => {
         await questionsListService.deleteQuestion(id);
         questionArrayFunc();
     }
+
+    // Get current posts
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = data.slice(indexOfFirstPost, indexOfLastPost);
+
+    // Change page
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     return (
         <>
@@ -48,7 +58,7 @@ const QuestionsList = () => {
                     Add new question
                 </Button>
             </Flex>
-            {data.map((card, id) => {
+            {currentPosts.map((card, id) => {
                 return (
                     <Flex
                         direction={['column', 'row', 'row']}
@@ -61,7 +71,6 @@ const QuestionsList = () => {
                         p="25px"
                         mb="25px"
                     >
-                        {console.log(card)}
                         <Flex direction="column">
                             <Text fontWeight="700" fontSize="14px">
                                 Question {id + 1}
@@ -104,6 +113,12 @@ const QuestionsList = () => {
                     </Flex>
                 );
             })}
+            <PaginationQuestionsList
+                postsPerPage={postsPerPage}
+                totalPosts={data.length}
+                paginate={paginate}
+                currentPage={currentPage}
+            />
         </>
     );
 };
